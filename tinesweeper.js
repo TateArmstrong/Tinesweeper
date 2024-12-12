470bfa0301b70a3ff02d0aa85fbb04cb71ecfd03
TateArmstrong <90807397+TateArmstrong@users.noreply.github.com>
2024-12-11 21:42:10 -0600

    ✨ Added numbers to cells.

--- tinesweeper.js
+++ tinesweeper.js
@@ -1,11 +1,59 @@
 var board = document.getElementById("board");
 
-var BOARD_WIDTH = 8;
-var BOARD_HEIGHT = 8;
+var BOARD_WIDTH = 16;
+var BOARD_HEIGHT = 16;
 var MINE_CHANCE = 0.15;
 
 var cellReferences = new Array(BOARD_HEIGHT);
 var cells = new Array(BOARD_HEIGHT);
+
+function onCellClick(event){
+    // There's got to be an easier way of getting the element reference. 
+    for(var i = 0; i < cells.length; i++){
+    for(var j = 0; j < cells[i].length; j++){
+        if(cells[i][j].htmlElement == this){
+
+            this.innerText = cells[i][j].number;
+        }
+    }
+    }
+}
+
+function calcCellNumbers() {
+    for(var i = 0; i < cells.length; i++){
+    for(var j = 0; j < cells[i].length; j++){
+        if(cells[i][j].hasBomb){
+            cells[i][j].number = -1;
+        }
+        else {
+            cells[i][j].number = getNumber(i, j);
+        }
+    }
+    }
+}
+
+function getNumber(row, col){
+    var count = 0;
+    for(var i = -1; i < 2; i++){
+    for(var j = -1; j < 2; j++){
+        // Skip if current cell. 
+        if(i == 0 && j == 0){
+            continue;
+        }
+        if((row + i) < 0 || (row + i) >= BOARD_HEIGHT || (col + j) < 0 || (col + j) >= BOARD_WIDTH){
+            continue;
+        }
+        if(cells[row + i][col + j].hasBomb){
+            count++;
+        }
+    }
+    }
+    return count;
+}
+
+function reveal() {
+
+}
 
 // Create cells. 
 for(var i = 0; i < cellReferences.length; i++){
@@ -15,14 +63,13 @@ for(var i = 0; i < cellReferences.length; i++){
         const cell = document.createElement("div");
         cell.classList.add("cell");
         board.appendChild(cell);
+        cell.addEventListener("click", onCellClick);
         cellReferences[i][j] = cell;
         cells[i][j] = {
             revealed: false,
             hasBomb: Math.random() < MINE_CHANCE,
+            number: -2,
             htmlElement: cell
-        }
-        if(cells[i][j].hasBomb){
-            cells[i][j].htmlElement.innerText = "💣";
         }
     }
 }
@@ -30,3 +77,5 @@ for(var i = 0; i < cellReferences.length; i++){
 // Create the grid. 
 board.style.gridTemplateColumns = `repeat(${BOARD_WIDTH}, minmax(0, 1fr))`;
 board.style.gridTemplateRows = `repeat(${BOARD_HEIGHT}, minmax(0, 1fr))`;
+
+calcCellNumbers();
